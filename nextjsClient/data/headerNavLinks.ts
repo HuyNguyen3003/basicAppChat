3ce02f320ac0bb29a3242b82b1c2b31444c5e735
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2023-09-02 02:10:11 +0700

    update

---
+++ nextjsClient/data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+  { href: '/login', title: 'Login' },
+]
+
+export default headerNavLinks
---
+++ nextjsClient/data/projectsData.ts
@@ -0,0 +1,12 @@
+const projectsData = [
+  {
+    title: 'AppChat Realtime Basic',
+    description: `Một ứng dụng giúp bạn nhắn tin với bạn bè, người thân qua việt tạo phòng. Không chỉ vậy 
+    chung tôi còn cung cập cho bạn một số tính năng giúp tăng trải nghiệm người giúp tốt hơn. 
+    Còn gì để suy nghĩ bấm vào và sử dụng ngay nào.`,
+    imgSrc: '/static/images/pjChatApp.png',
+    href: '/projects/appChat',
+  },
+]
+
+export default projectsData
