4a45bbe23799e3f4958532933fc8f0e41a10cf4a
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2023-09-07 21:51:58 +0700

    done res api asp.net and connect mongoDB

--- mcroservicesBe/aspUpdate/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace aspUpdate.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- mcroservicesBe/aspUpdate/Models/mongoDBSetting.cs
+++
@@ -1,8 +0,0 @@
-namespace MongoExample.Models;
-
-    public class mongoDBSetting
-    {
-        public string ConnectionURI { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string CollectionseName { get; set; } = null!;
-    }
